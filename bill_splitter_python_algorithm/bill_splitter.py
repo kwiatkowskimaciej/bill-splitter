f2976fa73b64b3ec4d79ee5fb8ce86b0e568006b
maciejkwiatkowski <maciek.kwiatkowski@outlook.com>
2023-07-29 12:13:52 +0200

    Edit bill_splitter.py

--- bill_splitter_python_algorithm/bill_splitter.py
+++ bill_splitter_python_algorithm/bill_splitter.py
@@ -1,4 +1,4 @@
-def transactions_maker(list):
+def bill_splitter(list):
     total = sum(list)
     items = len(list)
     avg = total / items
@@ -16,41 +16,18 @@ def transactions_maker(list):
                 diff_list[i] += diff_list[j]
                 diff_list[j] = 0
                 if amount > 0:
-                    transactions.append((i, j, round(amount, 2)))
+                    transactions.append((i, j, amount))
                 j -= 1
             else:
                 amount = round(-diff_list[i], 2)
                 diff_list[j] += diff_list[i]
                 diff_list[i] = 0
                 if amount > 0:
-                    transactions.append((i, j, round(amount, 2)))
+                    transactions.append((i, j, amount))
         i += 1
 
-    return transactions, len(transactions), (pos_count, neg_count), len(list)
-
-    # for i in range(neg_count):
-    #     while diff_list[i] < 0:
-    #         if diff_list[-1] == 0:
-    #             diff_list.pop()
-    #         else:
-    #             if -diff_list[i] > diff_list[-1]:
-    #                 diff_list[i] += diff_list[-1]
-    #                 amount = round(diff_list[-1], 2)
-
-    #                 if amount > 0:
-    #                   transactions.append((i, len(diff_list)-1, amount))
-
-    #                 diff_list[-1] = 0
-    #             else:
-    #                 diff_list[-1] = diff_list[-1] + diff_list[i]
-    #                 amount = round(-diff_list[i], 2)
-
-    #                 if amount > 0:
-    #                   transactions.append((i, len(diff_list)-1, amount))
-
-    #                 diff_list[i] = 0
-    # return transactions
+    return transactions
 
 lst = [71, 159, 142, 100, 69, 100, 12, 2]
 
-print(transactions_maker(lst))
+print(bill_splitter(lst))
