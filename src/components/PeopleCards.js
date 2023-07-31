98d113a4e5227ef087d572a3f6d157dc1f61281d
maciejkwiatkowski <maciek.kwiatkowski@outlook.com>
2023-07-31 23:08:11 +0200

    Fix bug with cards positioning and animation

--- src/components/PeopleCards.js
+++ src/components/PeopleCards.js
@@ -1,42 +1,51 @@
-import { useReducer, useState } from "react";
-import CardPlaceholder from "./CardPlaceholder";
+import { useEffect, useReducer, useState } from 'react';
+import CardPlaceholder from './CardPlaceholder';
 
 function reducer(state, action) {
   if (action.type === 'increment_people') {
     return {
-      numberOfPeople: state.numberOfPeople + 1
-    }
+      numberOfPeople: state.numberOfPeople + 1,
+    };
   }
-  throw Error('Unknown action.')
-}
-
-function positionReducer(card, action) {
-  if (action.type === 'change_position') {
-    return {
-      position: card.position - 200
-    }
-  throw Error('Unknown action')
-}
+  throw Error('Unknown action.');
 }
 
 export default function PeopleCards() {
-  const [calculatorOpen, setCalculatorOpen] = useState(false)
-  const [card, cardDispatch] = useReducer(positionReducer, {position: 300})
-  const [state, dispatch] = useReducer(reducer, { numberOfPeople: 0 })
+  const [calculatorOpen, setCalculatorOpen] = useState(false);
+  const [state, dispatch] = useReducer(reducer, { numberOfPeople: 0 });
+  const [firstCardPosition, setFirstCardPosition] = useState(300);
+  const [secondCardPosition, setSecondCardPosition] = useState(300);
 
-  function handleClick() {
-    dispatch({ type: 'increment_people'})
-    cardDispatch({ type: 'change_position'})
-  }
+  useEffect(() => {
+    if (calculatorOpen) {
+      setFirstCardPosition(100);
+      setSecondCardPosition(16);
+    }
+  }, [calculatorOpen]);
+
+  const handleClick = () => {
+    dispatch({ type: 'increment_people' });
+  };
 
   return (
     <div className="w-screen h-[calc(100vh-60px)] absolute bottom-0 overflow-hidden">
-      <div className={`w-screen h-screen bg-tertiary absolute top-[16px] rounded-3xl drop-shadow-[0_4px_16px_rgba(0,0,0,0.25)] px-6 pt-7`} hidden={!calculatorOpen}>
-        <p className="font-['Roboto'] text-xl font-medium text-[#ffffff]">Total: $665</p>
-      </div>
-      <div className={`w-screen h-full bg-secondary absolute rounded-t-3xl top-[${card.position}px] drop-shadow-[0_4px_16px_rgba(0,0,0,0.25)] px-6 pt-7`}
+      <div
+        className={`transition-[top] ease-out duration-500 delay-500 w-screen h-screen bg-tertiary absolute rounded-3xl drop-shadow-[0_4px_16px_rgba(0,0,0,0.25)] px-6 pt-7`}
+        style={{ top: `${secondCardPosition}px` }}
+        hidden={!calculatorOpen}
       >
-        <CardPlaceholder setCalculatorOpen={setCalculatorOpen} changeNumberOfPeople={handleClick}/>
+        <p className="font-['Roboto'] text-xl font-medium text-[#ffffff]">
+          Total: $665
+        </p>
+      </div>
+      <div
+        className={`transition-[top] ease-out duration-500 w-screen h-full bg-secondary absolute top-[300px] rounded-t-3xl drop-shadow-[0_4px_16px_rgba(0,0,0,0.25)] px-6 pt-7`}
+        style={{ top: `${firstCardPosition}px` }}
+      >
+        <CardPlaceholder
+          setCalculatorOpen={setCalculatorOpen}
+          changeNumberOfPeople={handleClick}
+        />
         <p>Number of people: {state.numberOfPeople}</p>
       </div>
       <div className="absolute bottom-[20px] left-0 right-0 flex justify-center mx-auto px-6 text-[#ffffff]">
@@ -47,7 +56,7 @@ export default function PeopleCards() {
         >
           Calculate
         </button>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
