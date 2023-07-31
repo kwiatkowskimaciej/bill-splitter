f7c58d6c42dc9b368dd1a0ce0a42d83cf3b9a336
maciejkwiatkowski <maciek.kwiatkowski@outlook.com>
2023-07-31 20:23:57 +0200

    Add animating cards

--- src/components/CardPlaceholder.js
+++ src/components/CardPlaceholder.js
@@ -1,6 +1,7 @@
-export default function CardPlaceholder() {
+export default function CardPlaceholder(props) {
   return (
-    <div className="border-dashed border-[1px] border-black rounded-xl h-[109px] flex items-center justify-center">
+    <div className="border-dashed border-[1px] border-black rounded-xl h-[109px] flex items-center justify-center"
+    onClick={() => props.setCalculatorOpen(true)}>
       <p className="text-center">+ Add person</p>
     </div>
   )
--- src/components/PeopleCards.js
+++ src/components/PeopleCards.js
@@ -1,15 +1,31 @@
+import { useEffect, useState } from "react";
 import CardPlaceholder from "./CardPlaceholder";
 
 export default function PeopleCards() {
+  const [calculatorOpen, setCalculatorOpen] = useState(false)
+  const [firstCardPosition, setFirstCardPosition] = useState(300)
+  const [secondCardPosition, setSecondCardPosition] = useState(300)
+
+  useEffect(() => {
+    if (calculatorOpen) {
+      setFirstCardPosition(100)
+      setSecondCardPosition(16)
+    }
+  }, [calculatorOpen])
+
   return (
-    <div className="w-screen h-screen absolute inset-0 overflow-hidden">
-      <div className="w-screen h-screen bg-secondary absolute top-[300px] rounded-3xl drop-shadow-[0_4px_16px_rgba(0,0,0,0.25)] px-6 pt-7">
-        <CardPlaceholder />
+    <div className="w-screen h-[calc(100vh-60px)] absolute bottom-0 overflow-hidden">
+      <div className={`transition-[top] ease-out duration-500 delay-500 w-screen h-screen bg-tertiary absolute top-[${secondCardPosition}px] rounded-3xl drop-shadow-[0_4px_16px_rgba(0,0,0,0.25)] px-6 pt-7`} hidden={!calculatorOpen}>
+        <p className="font-['Roboto'] text-xl font-medium text-[#ffffff]">Total: $665</p>
+      </div>
+      <div className={`transition-[top] ease-out duration-500 w-screen h-full bg-secondary absolute rounded-t-3xl top-[${firstCardPosition}px] drop-shadow-[0_4px_16px_rgba(0,0,0,0.25)] px-6 pt-7`}>
+        <CardPlaceholder setCalculatorOpen={setCalculatorOpen}/>
       </div>
       <div className="absolute bottom-[20px] left-0 right-0 flex justify-center mx-auto px-6 text-[#ffffff]">
         <button
           type="button"
-          className="font-['Montserrat'] font-semibold bg-tertiary w-full h-[56px] rounded-full"
+          disabled={!calculatorOpen}
+          className="font-['Montserrat'] font-semibold bg-tertiary w-full h-[56px] rounded-full disabled:bg-[#1D1B20] disabled:opacity-[0.12]"
         >
           Calculate
         </button>
