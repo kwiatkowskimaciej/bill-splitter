4d91365ba3e5bb95245b1dc5139c2810fc909061
maciejkwiatkowski <maciek.kwiatkowski@outlook.com>
2023-07-31 22:39:13 +0200

    Add useReducer for counting people

--- src/components/CardPlaceholder.js
+++ src/components/CardPlaceholder.js
@@ -1,7 +1,12 @@
 export default function CardPlaceholder(props) {
+  const handleClick = () => {
+    props.setCalculatorOpen(true)
+    props.changeNumberOfPeople()
+  }
+
   return (
     <div className="border-dashed border-[1px] border-black rounded-xl h-[109px] flex items-center justify-center"
-    onClick={() => props.setCalculatorOpen(true)}>
+    onClick={handleClick}>
       <p className="text-center">+ Add person</p>
     </div>
   )
--- src/components/PeopleCards.js
+++ src/components/PeopleCards.js
@@ -1,25 +1,43 @@
-import { useEffect, useState } from "react";
+import { useReducer, useState } from "react";
 import CardPlaceholder from "./CardPlaceholder";
+
+function reducer(state, action) {
+  if (action.type === 'increment_people') {
+    return {
+      numberOfPeople: state.numberOfPeople + 1
+    }
+  }
+  throw Error('Unknown action.')
+}
+
+function positionReducer(card, action) {
+  if (action.type === 'change_position') {
+    return {
+      position: card.position - 200
+    }
+  throw Error('Unknown action')
+}
+}
 
 export default function PeopleCards() {
   const [calculatorOpen, setCalculatorOpen] = useState(false)
-  const [firstCardPosition, setFirstCardPosition] = useState(300)
-  const [secondCardPosition, setSecondCardPosition] = useState(300)
+  const [card, cardDispatch] = useReducer(positionReducer, {position: 300})
+  const [state, dispatch] = useReducer(reducer, { numberOfPeople: 0 })
 
-  useEffect(() => {
-    if (calculatorOpen) {
-      setFirstCardPosition(100)
-      setSecondCardPosition(16)
-    }
-  }, [calculatorOpen])
+  function handleClick() {
+    dispatch({ type: 'increment_people'})
+    cardDispatch({ type: 'change_position'})
+  }
 
   return (
     <div className="w-screen h-[calc(100vh-60px)] absolute bottom-0 overflow-hidden">
-      <div className={`transition-[top] ease-out duration-500 delay-500 w-screen h-screen bg-tertiary absolute top-[${secondCardPosition}px] rounded-3xl drop-shadow-[0_4px_16px_rgba(0,0,0,0.25)] px-6 pt-7`} hidden={!calculatorOpen}>
+      <div className={`w-screen h-screen bg-tertiary absolute top-[16px] rounded-3xl drop-shadow-[0_4px_16px_rgba(0,0,0,0.25)] px-6 pt-7`} hidden={!calculatorOpen}>
         <p className="font-['Roboto'] text-xl font-medium text-[#ffffff]">Total: $665</p>
       </div>
-      <div className={`transition-[top] ease-out duration-500 w-screen h-full bg-secondary absolute rounded-t-3xl top-[${firstCardPosition}px] drop-shadow-[0_4px_16px_rgba(0,0,0,0.25)] px-6 pt-7`}>
-        <CardPlaceholder setCalculatorOpen={setCalculatorOpen}/>
+      <div className={`w-screen h-full bg-secondary absolute rounded-t-3xl top-[${card.position}px] drop-shadow-[0_4px_16px_rgba(0,0,0,0.25)] px-6 pt-7`}
+      >
+        <CardPlaceholder setCalculatorOpen={setCalculatorOpen} changeNumberOfPeople={handleClick}/>
+        <p>Number of people: {state.numberOfPeople}</p>
       </div>
       <div className="absolute bottom-[20px] left-0 right-0 flex justify-center mx-auto px-6 text-[#ffffff]">
         <button
