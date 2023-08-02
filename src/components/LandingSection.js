49cc51d90d84c07b60e018debc48865003fcebcd
maciejkwiatkowski <maciek.kwiatkowski@outlook.com>
2023-08-02 20:05:40 +0200

    Add image to landing section

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -2,14 +2,14 @@ export default function LandingSection() {
   return (
     <section className="bg-primary">
       <div
-        className="mx-auto flex flex-col max-w-7xl items-center justify-between p-6 lg:px-8"
+        className="mx-auto flex flex-col max-w-7xl items-center justify-between lg:px-8"
         aria-label="Global"
       >
-        <h1 className="font-['Roboto'] text-[#6B576A] text-center font-semibold text-5xl">
+        <h1 className="font-['Roboto'] text-[#6B576A] text-center font-semibold text-4xl px-8">
           Easily divide expenses
         </h1>
         <div>
-          {/* <img src={require("../images/hero-image.png")} /> */}
+          <img className="object-contain w-[200px]" src={require("../images/pig.webp")} />
         </div>
       </div>
     </section>
